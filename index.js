aa470d3341d481fa71ec3d576a224ee4fb6c7645
esther odedoyin <estherodedoyin1@gmail.com>
2025-05-15 17:56:28 +0100

    fixed delete product

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ const app=express()
 
 
 
-// app.use((req, res, next) => {
-//   if (req.headers.origin) {
-//     res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
-//   }
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-//   next();
-// });
+app.use((req, res, next) => {
+  if (req.headers.origin) {
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  }
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
  
 const corsOptions = {
   
